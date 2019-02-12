95865668a9287ee744d64ed0e4e67532e22ab1f0
“Serhyi <chief@ciat.net.ua>
2019-02-12 21:37:58 +0200

    hot fix

--- test_47.py
+++ test_47.py
@@ -1,11 +1,11 @@
-SSN_white_list = [1234, 2342, 2331, 6547]
+SSN_white_list = [1234, 2342, 2331, 6547, 44557]
 
 while True:
 
     user_ssn_number = input('Enter your SSN number in format (0000: )')
 
     # length checker
-    if len(user_ssn_number) != 4:
+    if len(user_ssn_number) != (4,5):
         print('Invalid SSN number, please check the format (0000)');
         continue
 
