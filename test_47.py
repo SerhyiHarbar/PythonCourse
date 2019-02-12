580613695549b735339e284f5e8e756c0bfc1ab1
“Serhyi <chief@ciat.net.ua>
2019-02-12 21:05:37 +0200

    refactor white list created

--- test_47.py
+++ test_47.py
@@ -1,10 +1,10 @@
-
-SSN_white_list = [1234, 2342, 2331]
+SSN_white_list = [1234, 2342, 2331, 6547]
 
 while True:
 
     user_ssn_number = input('Enter your SSN number in format (0000: )')
 
-    if len(user_ssn_number) !=4:
+    # length checker
+    if len(user_ssn_number) != 4:
         print('Invalid SSN number, please check the format (0000)');
         continue
